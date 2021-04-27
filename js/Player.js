ec74f6df4f02df35da5694772330a0e649c70948
ayasya771 <73102257+ayasya771@users.noreply.github.com>
2021-04-27 15:17:12 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,53 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = 0;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance,
+      rank: this.rank
+    });
+  }
+
+
+ getfinishedPlayers(){
+  var finishedPlayersref = database.ref('finishedPlayers');
+  finishedPlayersref.on("value",(data)=> {
+    finishedPlayers = data.val();
+  })
+ }
+
+ static updateFinishedPlayers(){
+   database.ref('/').update({
+     finishedPlayers : finishedPlayers + 1
+   });
+   this.rank += 1;
+ }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
+            
---
+++ sketch.js
@@ -0,0 +1,45 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers,finishedPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1,car2, car3, car4;
+var car1Img,car2Img,car3Img,car4Img,track,ground;
+
+function preload(){
+
+  car1Img = loadImage("images/car1.png");
+  car2Img = loadImage("images/car2.png");
+  car3Img = loadImage("images/car3.png");
+  car4Img = loadImage("images/car4.png");
+  track = loadImage("images/track.jpg");
+  ground = loadImage("images/ground.png");
+
+}
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+
+}
